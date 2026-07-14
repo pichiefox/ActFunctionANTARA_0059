61c91762ba374d2468785dc00e0efa0acb19dc8e
pichiefox <citrazad@gmail.com>
2026-07-14 10:23:08 +0700

    Conditional Statement If Else

--- IFELSE.cpp
+++ IFELSE.cpp
@@ -4,3 +4,12 @@ using namespace std;
 float HitungRerataBerparameter(float x, float y){
     return (x + y) / 2;
 }
+
+string CekStatusBerparameter(float a){
+    if (a >= 60){
+        return "Lulus";
+    }
+    else{
+        return "Tidak Lulus";
+    }
+}
