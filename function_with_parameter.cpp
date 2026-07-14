4f7cfadfc70a805923ae571bcd76f8d6d062c2ea
pichiefox <citrazad@gmail.com>
2026-07-14 10:27:46 +0700

    Membuat Fungsi dengan parameter

--- function_with_parameter.cpp
+++ function_with_parameter.cpp
@@ -2,3 +2,27 @@
 using namespace std;
 
 int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan Panjang :";
+    cin >> panjang;
+    cout << "Masukkan Lebar :";
+    cin >> lebar;
+}
+
+int luasPersegi(int a, int b)
+{
+    return a * b;
+}
+
+int kelilingPersegi(int a, int b)
+{
+    return 2 * (a + b);
+}
+
+void output ()
+{
+    cout << "Luasnya : " << luasPersegi(panjang, lebar) << endl;
+}
+
